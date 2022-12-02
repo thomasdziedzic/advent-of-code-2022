2dd4ffed685469e534633d7b51645bf3666376bd
Thomas Dziedzic <thomasdziedzic0@gmail.com>
2022-12-02 09:29:15 -0600

    day 1

---
+++ answers/day_1/problem.rb
@@ -0,0 +1,15 @@
+require 'active_support/all'
+
+class Problem
+  def initialize
+    @input_path = File.join(File.dirname(__FILE__), 'input')
+  end
+
+  def answer_part_1
+    File.readlines(@input_path).map { |line| line.chomp }.split('').map { |group| group.map(&:to_i).sum }.max
+  end
+
+  def answer_part_2
+    File.readlines(@input_path).map { |line| line.chomp }.split('').map { |group| group.map(&:to_i).sum }.sort.reverse.first(3).sum
+  end
+end
